f10d80372e1577fa956062f11f3bf9aa8a2de4bf
kneerose <duwalkneerose@gmail.com>
2020-01-19 23:16:39 +0545

    adding_all

--- app/src/main/java/com/example/smartagriculture/MainActivity.java
+++ app/src/main/java/com/example/smartagriculture/MainActivity.java
@@ -1,39 +1,31 @@
 package com.example.smartagriculture;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String JSON_URL = "http://duwalniraj.com.np/humidity.php";
-    //Integer[] temperature,humidity;
-    List<Feed> feedList;
-    ListView listView;
-    WebView webView;
+    Button on, off;
+    TextView tempv,humidityv,date,day;
+    DatabaseReference temp,hmdty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,16 +33,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-        listView=findViewById(R.id.topp);
-        WebView myWebView =  findViewById(R.id.webview);
+        on = findViewById(R.id.b1);
+        off = findViewById(R.id.b2);
+        String date_n = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(new Date());
+        date =findViewById(R.id.date);
+        date.setText(date_n);
+        String weekday_name = new SimpleDateFormat("EEEE", Locale.ENGLISH).format(System.currentTimeMillis());
+        day =findViewById(R.id.day);
+        day.setText(weekday_name);
+        tempv=findViewById(R.id.temperaturev);
+        humidityv=findViewById(R.id.humidityv);
+        temp= FirebaseDatabase.getInstance().getReference().child("temperature").child("Value");
+        temp.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String soil = dataSnapshot.getValue(String.class);
+                tempv.setText(soil);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_SHORT);
+            }
+        });
+        hmdty=FirebaseDatabase.getInstance().getReference().child("humidity").child("Value");
+        hmdty.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String hm=dataSnapshot.getValue(String.class);
+                humidityv.setText(hm);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_SHORT);
+            }
+        });
+        on.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("LED_STATUS");
+
+                myRef.setValue(1);
+
+            }
+        });
+        off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("LED_STATUS");
+
+                myRef.setValue(0);
+            }
+        });
+        WebView myWebView = findViewById(R.id.webview);
         myWebView.loadUrl("http://kalimatimarket.gov.np/daily-price-information");
         myWebView.setInitialScale(1);
         myWebView.getSettings().setLoadWithOverviewMode(true);
         myWebView.getSettings().setUseWideViewPort(true);
         myWebView.getSettings().setJavaScriptEnabled(true);
         myWebView.getSettings().setBuiltInZoomControls(true);
-        feedList=new ArrayList<>();
-        loadfeed();
         final SwipeRefreshLayout pullToRefresh = findViewById(R.id.pulltorefresh);
         pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -60,57 +107,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    public void refreshData()
-    {
 
-        listView=findViewById(R.id.topp);
-        WebView myWebView =  findViewById(R.id.webview);
+    public void refreshData() {
+        WebView myWebView = findViewById(R.id.webview);
         myWebView.loadUrl("http://kalimatimarket.gov.np/daily-price-information");
         myWebView.setInitialScale(1);
         myWebView.getSettings().setLoadWithOverviewMode(true);
         myWebView.getSettings().setUseWideViewPort(true);
         myWebView.getSettings().setJavaScriptEnabled(true);
         myWebView.getSettings().setBuiltInZoomControls(true);
-        feedList=new ArrayList<>();
-        loadfeed();
     }
-    public void goToSo (View view) {
-        goToUrl ( "http://kalimatimarket.gov.np/daily-price-information");
+
+    public void goToSo(View view) {
+        goToUrl("http://kalimatimarket.gov.np/daily-price-information");
     }
-    private void goToUrl (String url) {
+
+    private void goToUrl(String url) {
         Uri uriUrl = Uri.parse(url);
         Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
         startActivity(launchBrowser);
-    }
-    private void loadfeed() {
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, JSON_URL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONArray ja= new JSONArray(response);
-                           /* for (int i = 0; i < ja.length(); i++) {*/
-                                JSONObject feedObject = ja.getJSONObject(ja.length()-1);
-                                Feed data = new Feed(feedObject.getString("temperature"),
-                                       feedObject.getString("humidity"));
-                                feedList.add(data);
-                            //}
-                            CustomAdapter adapter = new CustomAdapter(feedList, getApplicationContext());
-                            listView.setAdapter(adapter);
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
     }
 }
